fc61d50c1466bd6a5496fbe0bb2591782d193655
Pichai Tangtrongsakundee <Pichai.t@gmail.com>
2024-09-29 01:51:04 +1000

    Init Commit the first twot test scenarios

---
+++ verifyValues_test.ts
@@ -0,0 +1,26 @@
+
+Feature('Verify Values for Asx Top 5');
+
+Scenario('Check Values for Top 5 Stocks(Overall) Tab',  ({ I }) => {
+   I.amOnPage('https://asxtop5.onrender.com/');      
+   I.click('//*[@id="root"]/div/div[2]/label[1]');
+   // Verify top 5 stock names (at specific locations)
+   I.see('WHC', '//*[@id="marzen"]/table/tbody/tr[1]/td[1]');
+   I.see('HDN', '//*[@id="marzen"]/table/tbody/tr[2]/td[1]');
+   I.see('HLI', '//*[@id="marzen"]/table/tbody/tr[3]/td[1]');
+   I.see('IGO', '//*[@id="marzen"]/table/tbody/tr[4]/td[1]');  
+   I.see('MTS', '//*[@id="marzen"]/table/tbody/tr[5]/td[1]');    
+   I.wait(2);  
+});
+
+Scenario('Check Values for 5 Highest Dividend Yields Tab',  ({ I }) => {
+   I.amOnPage('https://asxtop5.onrender.com/');      
+   I.click('#root > div > div.tabset > label:nth-child(4)');
+   // Verify 5 highest dividend yields (at specific locations)
+   I.see('10.94%','//*[@id="rauchbier"]/table/tbody/tr[1]/td[5]');
+   I.see('9.95%','//*[@id="rauchbier"]/table/tbody/tr[2]/td[5]');
+   I.see('8.94%','//*[@id="rauchbier"]/table/tbody/tr[3]/td[5]');
+   I.see('8.58%','//*[@id="rauchbier"]/table/tbody/tr[4]/td[5]');
+   I.see('8.52%','//*[@id="rauchbier"]/table/tbody/tr[5]/td[5]');
+});
+
